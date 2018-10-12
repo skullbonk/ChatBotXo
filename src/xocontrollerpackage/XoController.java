17804f81a3847b66e53b205975726210554242e3
Skullbonk <reagan.fairbanks@gmail.com>
2018-10-12 13:56:59 -0600

    did shiz

--- src/xocontrollerpackage/XoController.java
+++ src/xocontrollerpackage/XoController.java
@@ -16,6 +16,11 @@ public class XoController
 	public void start()
 	{
 		initialization();
+		String latestInput = "none";
+		while(!latestInput.equalsIgnoreCase("quit"))
+		{
+			chatSession();
+		}
 	}
 	
 	/**
@@ -24,24 +29,21 @@ public class XoController
 	public void initialization()
 	{
 		String disclaimer = JOptionPane.showInputDialog(null, "HHEY! I'm Xo. I say stupid crap. I don't save any data other than the name you give me, and I don't send that to anyone. You cool with that? (y/n)");
-		if(disclaimer == "n")
+		
+		while(!disclaimer.equalsIgnoreCase("y"))
 		{
-			JOptionPane.showMessageDialog(null, "Well what the frick are you doing here then? Go do something else!");
+			disclaimer = JOptionPane.showInputDialog(null, "Scuse me, kiddo, if you wanna get past this screen, you pretty much have to type y. Either try again or go do something else.");		
 		}
-		else
-		{
-			if(disclaimer == "y")
-			{
-				String userName = JOptionPane.showInputDialog(null, "Marvelous. You're gonna have to give me something to adress you by. You know, like a name. It doesn't have to be your actual name. It doesn't even matter what you type in here. It's just a variable that some nerd has to remember to fart back out at you now and then. Hurry up lad, this Mac isn't getting any faster.");
-				bot.setName(userName);
-			}
-			else
-			{
-				JOptionPane.showInputDialog(null, "Scuse me, kiddo, you gotta type in either y or n. Try again.");
-			}
-		}
+				
+		String userName = JOptionPane.showInputDialog(null, "Marvelous. You're gonna have to give me something to adress you by. Like a name. It doesn't have to be your actual name. It doesn't even matter what you type in here, It's just another variable to fart out.");
+		bot.setName(userName);
 	}
 	
+	public void chatSession()
+	{
+		JOptionPane.showMessageDialog(null, "Excellent. Welcome to Xo, " + bot.getName() + ".");
+		String latestInput = JOptionPane.showInputDialog(null, "What do you wanna say?");
+	}
 
 	
 

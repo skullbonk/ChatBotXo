95879ef632e589a0a1a341bc3e6926d0331ef7a1
Skullbonk <reagan.fairbanks@gmail.com>
2018-10-16 13:21:42 -0600

    Created bank of responses, still empty

--- src/xomodel/ChatBot.java
+++ src/xomodel/ChatBot.java
@@ -1,9 +1,18 @@
 package xomodel;
+import java.util.ArrayList;
 
 
 public class ChatBot
 {
 	private String name;
+	private String jokeAsk;
+	private String jokeAnswer;
+	private String dismissal;
+	
+	private ArrayList<String> responseBank;
+	private ArrayList<String> spookyStuff;
+	
+	
 	
 	/**
 	 * Initializes ChatBot with default settings.
@@ -11,6 +20,27 @@ public class ChatBot
 	public ChatBot()
 	{
 		
+		this.name = "N/A";
+		this.jokeAsk = "What do you call this joke?";
+		this.jokeAnswer = "Default.";
+		this.dismissal = "Wow that's great, let's talk about [topic]";
+	
+		
+		this.responseBank = new ArrayList<String>();
+		this.spookyStuff = new ArrayList<String>();
+		
+	}
+	
+	public ChatBot(String name, String jokeAsk, String jokeAnswer, String dismissal)
+	{
+		this.name = name;
+		this.jokeAsk = jokeAsk;
+		this.jokeAnswer = jokeAnswer;
+		this.dismissal = dismissal;
+	}
+	
+	private void responseList()
+	{
 		
 	}
 	
